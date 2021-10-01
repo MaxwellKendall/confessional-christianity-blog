73442543b93239d35618eded91fe7017767f2de3
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2021-10-01 16:32:30 -0400

    FOUT fix?

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
+import "@fontsource/cinzel-decorative/index.css"; // Weight 400.
+import "@fontsource/cinzel/index.css"; // Weight 400.
+import "@fontsource/marcellus/index.css";
+
 // normalize CSS across browsers
 import "./src/normalize.scss"
 // custom CSS styles
