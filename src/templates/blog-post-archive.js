f0fc3c80c98dff6c7650a562d0943e109bea9bf1
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2023-01-12 10:03:16 -0500

    add tags and search

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link, graphql, navigate } from "gatsby"
 import parse from "html-react-parser"
 
@@ -19,28 +19,29 @@ const handleFilter = (searchTerm) => {
 
 const URL_REGEX = /\s/g;
 const POST_REGEX = /,|'/g
+const DEFAULT_MAX = 10;
 
 const BlogIndex = ({
   data,
   location,
-  pageContext: { nextPagePath, previousPagePath },
 }) => {
   const queryParams = new URLSearchParams(location.search)
-  const search = queryParams.get('search') || '';
+  const search = queryParams.get('q') || '';
+  const [postLimit, setPostLimit] = useState(DEFAULT_MAX);
   const posts = data.allWpPost.nodes
   
   const handleChange = (e) => {
     e.persist();
-    console.log(e.target.value);
-    new URLSearchParams(e.target.value)
-    navigate(`/?search=${e.target.value.replaceAll(URL_REGEX, '%20')}`);
+    navigate(`/?q=${e.target.value.replaceAll(URL_REGEX, '%20')}`);
   }
   
-  const handleKeyDown = (e) => {
-      e.persist();
-    if (e.keyCode === 13) {
-      navigate(`/?search=${search}`);
-    }
+  const handleLoadMore = (e) => {
+    e.preventDefault();
+    setPostLimit(postLimit + DEFAULT_MAX)
+  }
+  
+  const handleClickTag = (name) => {
+    navigate(`/?q=${name}`)
   }
 
   if (!posts.length) {
@@ -51,54 +52,67 @@ const BlogIndex = ({
       </Layout>
     )
   }
+  
+  const filteredPosts = posts
+  .filter((post) => {
+      if (!search) return true;
+      const cleanedTitle = post.title.toLowerCase().replaceAll(POST_REGEX, '');
+      const cleanedExcerpt = post.excerpt.toLowerCase().replace(POST_REGEX, '');
+      if (post.tags.nodes.some(({ name }) => name.toLowerCase().includes(search.toLowerCase()))) return true;
+      if (post.title.toLowerCase().includes(search.toLowerCase()) || cleanedTitle.includes(search.toLowerCase())) return true;
+      if (post.excerpt.toLowerCase().includes(search.toLowerCase()) || cleanedExcerpt.includes(search.toLowerCase())) return true;
+      return false;
+    })
 
   return (
     <Layout isHomePage>
       <SEO title="All posts" />
       <div className="w-full flex justify-center">
-        <input className="p-2 rounded-md border-2 mx-auto" placeholder="Search posts" type="text" value={search} onKeyDown={handleKeyDown} onChange={handleChange} />
+        <input className="p-2 rounded-md border-2 mx-auto" placeholder="Search posts" type="text" value={search} onChange={handleChange} />
       </div>
       <ol className="list-none">
-        {posts.filter(
-          (post) => {
-            if (!search) return true;
-            const cleanedTitle = post.title.toLowerCase().replaceAll(POST_REGEX, '');
-            const cleanedExcerpt = post.excerpt.toLowerCase().replace(POST_REGEX, '');
-            
-            if (post.title.toLowerCase().includes(search.toLowerCase()) || cleanedTitle.includes(search.toLowerCase())) return true;
-            if (post.excerpt.toLowerCase().includes(search.toLowerCase()) || cleanedExcerpt.includes(search.toLowerCase())) return true;
-            return false;
-          })
-          .map(post => {
-          const title = post.title
-          return (
-            <li key={post.uri} className="my-12 md:my-24">
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.uri} itemProp="url">
-                      <span itemProp="headline">{parse(title)}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.date}</small>
-                </header>
-                <section itemProp="description">{parse(post.excerpt)}</section>
-              </article>
-            </li>
-          )
-        })}
+        {filteredPosts
+            .slice(0, postLimit)
+            .map(post => {
+              const title = post.title;
+              const tags = post.tags.nodes;
+              return (
+                <li key={post.uri} className="my-12 md:my-24">
+                  <article
+                    className="post-list-item"
+                    itemScope
+                    itemType="http://schema.org/Article"
+                  >
+                    <header>
+                      <h2>
+                        <Link to={post.uri} itemProp="url">
+                          <span itemProp="headline">{parse(title)}</span>
+                        </Link>
+                      </h2>
+                      <small>{post.date}</small>
+                    </header>
+                    <section itemProp="description">{parse(post.excerpt)}</section>
+                    <ul className="flex mt-2">
+                      {
+                        tags.map(({ name }, i) => {
+                          // possibly return more stuff here :shrug:?
+                          return (
+                            <li onClick={(e) => handleClickTag(name)} className="cursor-pointer border-2 py-2 px-4 md:p-4 uppercase bold text-sm">
+                              {name}
+                            </li>
+                          )
+                        })  
+                      }
+                    </ul>
+                  </article>
+                </li>
+              )
+            })
+          }
       </ol>
-      {previousPagePath && (
-        <>
-          <Link to={previousPagePath}>Previous page</Link>
-          <br />
-        </>
+      {filteredPosts.length > postLimit && (
+          <button className="flex mx-auto p-10" onClick={handleLoadMore}>LOAD MORE</button>
       )}
-      {nextPagePath && <Link to={nextPagePath}>Next page</Link>}
     </Layout>
   )
 }
@@ -106,11 +120,9 @@ const BlogIndex = ({
 export default BlogIndex
 
 export const pageQuery = graphql`
-  query WordPressPostArchive($offset: Int!, $postsPerPage: Int!) {
+  query WordPressPostArchive {
     allWpPost(
       sort: { fields: [date], order: DESC }
-      limit: $postsPerPage
-      skip: $offset
     ) {
       nodes {
         excerpt
@@ -118,6 +130,11 @@ export const pageQuery = graphql`
         date(formatString: "MMMM DD, YYYY")
         title
         excerpt
+        tags {
+					nodes {
+						name
+          }
+        }
       }
     }
   }
